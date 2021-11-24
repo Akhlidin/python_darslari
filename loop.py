4e34fd27e5ba96137b86445c726b4506a35ed6b1
Akhlidin Shermatov <94664731+Akhlidin@users.noreply.github.com>
2021-11-24 10:08:23 +0500

    loop
    2021.11.23

---
+++ loop.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Axlidin Shermatov Sharobiddin o'g'li
+Created on Fri Oct 29 10:36:19 2021
+
+@author: Axlidin
+"""
+
+# mehmonlar = ["ali", "Axlidin", "Xusniddin", "Nuriddin",  "Vali"]
+# for mehmon in mehmonlar:
+#     print("salom", mehmon)
+# print("xayr,", mehmon)
+# mehmonlar = ["ali", "Axlidin", "Xusniddin", "Nuriddin",  "Vali"]
+# for mehmon in mehmonlar:
+#     print(f" Assalomu alaykum xurmatli {mehmon}, sizni 13-dekabr kuni naxorgi oshga taklif qlaman")
+#     print("xurmart bilan Sharobiddin aka xonadoni\n")
+# sonlar = list(range(0, 110, 10))
+# for son in sonlar:
+# #     print(f" {son} ning kvadrati {son**2} ga teng")
+# sonlar = list(range(11))
+# sonlar_kvadrati = []
+# for son in sonlar:
+#     sonlar_kvadrati.append(son**2)
+# print(sonlar)
+# print(sonlar_kvadrati)
+# friends = []
+# print("5 ta eng yaqin do'stingizni kim?")
+# for n in range(5):
+#     friends.append(input(f"{n+1}-do'stingizni ismini yozing: "))
+# print(friends)
+                   
+#amaliyot:
+# ismlar = ['asadbek', 'Axadbek', 'Shermuhammadjon', 'Ruxshonabonu', 'alo']
+# for ism in ismlar:
+#     print(f"salom hurmatli jiyanim {ism} yaxshi keldingmi")
+#     print("xayr") 
+# sonlar = list(range(11, 100, 2))
+# for son in sonlar:
+#     print(f"{son**3}")
+# kinolar = []
+# print("eng yoqtirgan 5 tasini nomini yozingmaave")
+# for kino in range(5):
+#     kinolar.append(input(f"{kino+1}-kino nominiyozing: "))
+# print(kinolar)
+# n_people =int(input("bugun nechta odam bilan suxbatlashdingiz?>>>"))
+# ismlar = []
+# for n in range(n_people):
+#     ismlar.append(input(f"{n+1}-suhbat qilgan odamizni ismini yozing: "))
+# print(ismlar)
